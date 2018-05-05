1269dc3c3b955289beb30283b2fe285078406238
Managersky <witoldmetel@gmail.com>
2018-05-05 10:49:46 +0200

    prepare spotify service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -7,6 +9,7 @@ import { AboutComponent } from './components/about/about.component';
 import { SearcherComponent } from './components/searcher/searcher.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SpotifyService } from './services/spotify.service';
 
 @NgModule({
   declarations: [
@@ -17,9 +20,12 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule
   ],
-  providers: [],
+  providers: [ SpotifyService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/searcher/searcher.component.ts
+++ src/app/components/searcher/searcher.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-searcher',
   templateUrl: './searcher.component.html',
-  styleUrls: ['./searcher.component.css']
+  styleUrls: ['./searcher.component.css'],
+  providers: [SpotifyService]
 })
 export class SearcherComponent implements OnInit {
+  searcher: string;
 
-  constructor() { }
+  searchMusic() {
+    this._spotifyService.searchMusic(this.searcher)
+      .subscribe(res => {
+        console.log(res.artists.items);
+      })
+  }
+
+  constructor(private _spotifyService: SpotifyService) { }
 
   ngOnInit() {
   }
