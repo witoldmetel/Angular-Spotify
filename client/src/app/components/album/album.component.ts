f63ef99c0e1cfb37a634323d4c59b6635e2e5c14
Managersky <witoldmetel@gmail.com>
2018-05-12 19:08:46 +0200

    stylling album component

--- client/src/app/components/album/album.component.ts
+++ client/src/app/components/album/album.component.ts
@@ -12,6 +12,7 @@ export class AlbumComponent implements OnInit {
   accessToken: string;
   id: string;
   album: any;
+  track_duration: any;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -21,14 +22,31 @@ export class AlbumComponent implements OnInit {
 
     this.route.params
       .subscribe(params => {
-        console.log(params);
         spotify.getAlbum(params['id'])
           .then(res => {
             this.album = res;
-            console.log(this.album);
+            // console.log(this.album);
+            return this.album.tracks.items.map(track => {
+              return track.id
+            });
+          })
+          .then(trackId => {
+            return spotify.getTracks(trackId);
+          })
+          .then(trackInfo => {
+            return this.album.tracks.items.map(track => {
+              this.track_duration = this.convertDuration(track.duration_ms);
+              // console.log(this.track_duration);
+            });
           })
           .catch(err => console.error(err));
       })
+  }
+
+  convertDuration(ms) {
+    let min = Math.floor((ms / 1000 / 60) << 0);
+    let sec = Math.floor((ms / 1000) % 60);
+    return min + ":" + (sec < 10 ? '0' : '') + sec;
   }
 
 }
